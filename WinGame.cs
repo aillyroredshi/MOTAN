1970c2fe94d68ce76aef869a6e12e8f2d2854595
Artur <comanda.artur@gmail.com>
2016-05-29 22:14:53 +0300

    Win Game

---
+++ WinGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
