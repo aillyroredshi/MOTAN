6249dd9a179912ac9660dd71e1fb78b26f2c45d3
Artur <comanda.artur@gmail.com>
2016-05-29 22:12:55 +0300

    Home Button

---
+++ goHome.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class goHome : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
