edcd97934faba0b73551bdb1331112b0f8cc67f6
Artur <comanda.artur@gmail.com>
2016-05-29 22:13:31 +0300

    Menu

---
+++ MenuGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuGame : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
